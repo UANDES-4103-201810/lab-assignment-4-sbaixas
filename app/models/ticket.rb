ea1c86af71b02fd5aaa979ca0f20d0478f203ee4
sbaixas <sbaixas@gmail.com>
2018-04-03 14:56:38 -0300

    all done

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,5 +3,13 @@ class Ticket < ApplicationRecord
 	has_many :user_tickets
 	has_many :users, through: :user_tickets
 	validates :price, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+	validate :check_created_after_event, on: :create
+	
+	def check_created_after_event:
+		if self.event.start_date < Date.today
+			errors.add(:created_at, "event already started")		
+		end	
+	end
 
+	
 end
--- app/models/user_ticket.rb
+++ app/models/user_ticket.rb
@@ -1,4 +1,18 @@
 class UserTicket < ApplicationRecord
-  belongs_to :user
-  belongs_to :ticket
+	belongs_to :user
+	belongs_to :ticket
+	validate :check_bought_after_event, on: :create
+	validate :has_unpaid_ticket, on: :create
+	
+	def check_bought_after_event:
+		if self.ticket.event.start_date < Date.today
+			errors.add(:created_at, "event already started")		
+		end	
+	end
+
+	def check_bought_after_event:
+		if UserTicket.exists?(user_id: self.user, paid: false)
+			errors.add(:user_id, "user has unpayed tickets")
+		end
+	end
 end
